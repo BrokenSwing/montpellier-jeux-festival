9e4a6c0311449b8007032cbd95e9784d753e900e
BrokenSwing <fhugouvieux@gmail.com>
2021-03-11 09:21:52 +0100

    Remove unused imports

--- src/festival/festival.module.ts
+++ src/festival/festival.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { FestivalService } from './festival.service';
 import { FestivalController } from './festival.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Festival } from './entities/festival.entity';
-import { Price } from './entities/prices.entity';
-import { Area } from './entities/area.entity';
 import { PriceService } from './price.service';
 import { databaseAccessModule } from '../utils';
 
