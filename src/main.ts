b300330cbf8701b743e8af2d842fceb90219c663
BrokenSwing <fhugouvieux@gmail.com>
2021-03-15 19:27:14 +0100

    Add CORS and get port from env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       forbidUnknownValues: true,
@@ -31,6 +32,6 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
